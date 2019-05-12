b08d62162db8a1130c9c3c36f6a1786e9fbf0185
Softwarica College Lab <softwarica@softwarica.com>
2019-05-12 11:38:07 +0545

    post

--- app/src/main/java/com/example/apiretrofit/apiclient/EmployeeAPI.java
+++ app/src/main/java/com/example/apiretrofit/apiclient/EmployeeAPI.java
@@ -1,13 +1,29 @@
 package com.example.apiretrofit.apiclient;
 
 import com.example.apiretrofit.model.Employee;
+import com.example.apiretrofit.model.EmployeeCUD;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface EmployeeAPI {
     @GET("employees")
     Call<List<Employee>>getEmployee();
+
+    @POST ("create")
+    Call<Void> registerEmployee(@Body EmployeeCUD emp);
+
+
+
+
+
+    //Get employee on the basis of EmpID
+    @GET("employee/{empID}")
+    Call<Employee> getEmployeeByID(@Path("empID") int empId);
+
 }
